c615c356b498ba13d8605a6026dcf54aac9ca75c
Tangent Chang <andy23512@gmail.com>
2026-07-11 13:23:31 +0800

    chore: remove unused codes

--- src/app/utils/layout.utils.ts
+++ src/app/utils/layout.utils.ts
@@ -1,8 +1,6 @@
-import { TranslateService } from '@ngx-translate/core';
 import { ACTIONS, ActionType, KeyboardLayout, Layer } from 'tangent-cc-lib';
 import { ACTION_REPRESENTATION_ICON_MAP } from '../data/action-representation-icon-map';
 import { ChordKey } from '../models/chord.models';
-import { toTitleCase } from './case.utils';
 
 export function getChordKeyFromActionCode(
   actionCode: number,
@@ -27,50 +25,6 @@ export function getChordKeyFromActionCode(
     }
     return { type: 'icon', value: icon };
   }
-}
-
-export function convertPositionCodeToText(
-  positionCode: number,
-  translateService: TranslateService,
-) {
-  const hand = positionCode < 45 ? 'hand.left' : 'hand.right';
-  const sw =
-    'switch.' +
-    [
-      'thumb-back',
-      'thumb-middle',
-      'thumb-front',
-      'index',
-      'middle',
-      'ring',
-      'pinky',
-      'middle-secondary',
-      'ring-secondary',
-    ][Math.floor((positionCode % 45) / 5)];
-  const direction = (
-    hand === 'hand.left'
-      ? [
-          'direction.down',
-          'direction.east',
-          'direction.north',
-          'direction.west',
-          'direction.south',
-        ]
-      : [
-          'direction.down',
-          'direction.west',
-          'direction.north',
-          'direction.east',
-          'direction.south',
-        ]
-  )[positionCode % 5];
-  return toTitleCase(
-    translateService.instant('layout-text-guide', {
-      hand: translateService.instant(hand),
-      switch: translateService.instant(sw),
-      direction: translateService.instant(direction),
-    }),
-  );
 }
 
 export function getHoldKeys(
